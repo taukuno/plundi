4deec23fb6867a0e227757735b3aa4a1140fdb88
Tobias Herzog <tobias.rm.herzog@outlook.com>
2025-01-28 15:24:41 +0100

    Remove hotfixed effect from Aura of Zealotry

--- src/Plundi.Hammerfall.App/BuildInfo.cs
+++ src/Plundi.Hammerfall.App/BuildInfo.cs
@@ -11,6 +11,6 @@ namespace Plundi.Hammerfall.App
 {
     public static class BuildInfo
     {
-        public static string LastUpdate = "2025-01-25 22:02 UTC";
+        public static string LastUpdate = "2025-01-28 14:23 UTC";
     }
 }
